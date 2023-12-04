46499af5b77a7d6c283ea15a9a26e12b933ad45f
arnaudstiegler <arnaud.stiegler@gmail.com>
2023-12-04 18:26:19 -0500

    added basic shading

--- rasterizer_playground/rasterize_triangle.py
+++ rasterizer_playground/rasterize_triangle.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from rasterizer_playground.perspective_transform import perspective_project
 
-def sign(p1, p2, p3) -> float:
+def edge_function(p1, p2, p3) -> float:
     '''
     Function that returns the signed area of the parallelogram defined by p1p3 and p2p3
     '''
@@ -14,15 +14,17 @@ def is_in_triangle(point, triangle):
     Compute the signed area for all parallelograms defined by the point and a pair of the triangle vertices
     If signs are all the same, it means the point is "on the same side" of every triangle edge, ie. within the triangle
     '''
-    sign1 = sign(point, triangle[0], triangle[1])
-    sign2 = sign(point, triangle[1], triangle[2])
-    sign3 = sign(point, triangle[2], triangle[0])
+    e1 = edge_function(point, triangle[0], triangle[1])
+    e2 = edge_function(point, triangle[1], triangle[2])
+    e3 = edge_function(point, triangle[2], triangle[0])
+    triangle_area = edge_function(*triangle)
 
-    has_neg = sign1<0 or sign2<0 or sign3 < 0
-    has_pos = sign1>0 or sign2>0 or sign3>0
-    return not (has_neg and has_pos)
+    has_neg = e1<0 or e2<0 or e3 < 0
+    has_pos = e1>0 or e2>0 or e3>0
+    return not (has_neg and has_pos), [e1/triangle_area,e2/triangle_area,e3/triangle_area]
 
 triangle = np.array([[0.0,0.0,0.0], [10.0,0.0,0.0], [0.0,5.0,0.0]])
+triangle_colors = np.array([[255,0.0,0.0], [0.0,255,0.0], [0.0,0.0,255]])
 
 camera_args = {
     # No rotation, just "stepping back" from the origin
@@ -36,27 +38,35 @@ camera_args = {
     # This is an approximation, and canvas_dist is usually defined by FOV
     'canvas_dist': 1,
     # Screen properties
-    'width': 100,
-    'height': 100,
-    'pixel_width': 1.0,
-    'pixel_height': 1.0
+    'width': 25,
+    'height': 25,
+    'pixel_width': 0.01,
+    'pixel_height': 0.01
 }
 
 raster_triangle = perspective_project(triangle, camera_args)
 
-screen = np.zeros((int(camera_args['width'] / camera_args['pixel_width']), int(camera_args['height'] / camera_args['pixel_height'])))
+screen = np.zeros((int(camera_args['width'] / camera_args['pixel_width']), int(camera_args['height'] / camera_args['pixel_height']), 3))
 
 # Find the bbox for the triangle so that we don't uselessly go over pixels
 bbox = np.concatenate([np.min(raster_triangle[:, :2], axis=0), np.max(raster_triangle[:, :2], axis=0)]).astype(int)
 
 for x_value in range(bbox[0], bbox[2]):
     for y_value in range(bbox[1], bbox[3]):
-        # For each
-        if is_in_triangle([x_value, y_value], raster_triangle):
-            screen[x_value, y_value] = 1
+        is_in, barycentric_weights = is_in_triangle([x_value, y_value], raster_triangle)
+        if is_in:
+            # Assign color as a weighted sum of the colors of the 3 vertices
+            screen[x_value, y_value, :] = (
+                barycentric_weights[0]*triangle_colors[0] + 
+                barycentric_weights[1]*triangle_colors[1] +
+                barycentric_weights[2]*triangle_colors[2] 
+                )
 
 # Marking the triangle vertices another color
-screen[raster_triangle[:,0], raster_triangle[:,1]] = 0.5
+screen[raster_triangle[:,0], raster_triangle[:,1]] = triangle_colors
+
+# For matplotlib to not clip the color values, have to turn into integers
+screen = screen.astype(int)
 
 plt.figure(figsize=(10, 10), dpi=100)  # Adjust the figure size and dpi as needed
 plt.imshow(screen)
